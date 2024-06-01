53c54ce8effb13462e39ca7e7beb607f0a0e7ac8
SarnojitNaskar <123971203+SarnojitNaskar@users.noreply.github.com>
2024-06-01 23:27:34 +0530

    Create bin_bcd_dig.v

---
+++ bin_bcd_dig.v
@@ -0,0 +1,10 @@
+module bin_bcd_dig(
+    input A3, input A2, input A1, input A0,
+    output B4, output B3, output B2, output B1, output B0
+    );
+    assign B4 = A3&(A2|A1);
+    assign B3 = A3&(~A2)&(~A1);
+    assign B2 = ((~A3)|A1)&A2;
+    assign B1 = ((~A3)&A1)|(A3&A2&(~A1));
+    assign B0 = A0;
+endmodule
