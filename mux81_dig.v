37e5a678e642f19f3b0f0eaeab77fb7b88616eb3
SarnojitNaskar <123971203+SarnojitNaskar@users.noreply.github.com>
2024-06-01 22:52:54 +0530

    Create mux81_dig.v

---
+++ mux81_dig.v
@@ -0,0 +1,8 @@
+module mux81_dig(
+    input s0, input s1, input s2,
+    input d0, input d1, input d2, input d3, input d4, input d5, input d6, input d7,
+    output y
+    );
+    assign y = (d0&~s2&~s1&~s0) | (d1&~s2&~s1&s0)| (d2&~s2&s1&~s0)| (d3&~s2&s1&s0)
+                | (d4&s2&~s1&~s0)| (d5&s2&~s1&s0)| (d6&s2&s1&~s0)| (d7&s2&s1&s0);
+endmodule
