7d43ece402629711763f16db4567d8b89870eae8
SarnojitNaskar <123971203+SarnojitNaskar@users.noreply.github.com>
2024-06-01 23:40:33 +0530

    Create bin_ex3_dig.v

---
+++ bin_ex3_dig.v
@@ -0,0 +1,9 @@
+module bin_ex3_dig(
+    input [3:0]b, output [4:0]e
+    );
+    assign e[4]=(b[3]&b[2]&b[0])|(b[3]&b[2]&b[1]);
+    assign e[3]=(b[3]&~b[2])|(b[3]&~b[1]&~b[0])|(~b[3]&b[2]&b[0])|(~b[3]&b[2]&b[1]);
+    assign e[2]=(~b[2]&b[0])|(~b[2]&b[1])|(b[2]&~b[1]&~b[0]);
+    assign e[1]=(~b[1]&~b[0])|(b[1]&b[0]);
+    assign e[0]=~b[0];
+endmodule
