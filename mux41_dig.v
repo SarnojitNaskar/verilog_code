723747c4626995cf8b119c1d1f150a259ed17650
SarnojitNaskar <123971203+SarnojitNaskar@users.noreply.github.com>
2024-06-01 22:50:14 +0530

    Create mux41_dig.v

---
+++ mux41_dig.v
@@ -0,0 +1,7 @@
+module mux41_dig(
+    input s0, input s1,
+    input d0, input d1, input d2, input d3,
+    output y
+    );
+    assign y = (d0&~s1&~s0) | (d1&~s1&s0) | (d2&s1&~s0) | (d3&s1&s0);
+endmodule
