b6408b91b38ed224d311d174e90e9462379f3349
SarnojitNaskar <123971203+SarnojitNaskar@users.noreply.github.com>
2024-06-01 21:17:29 +0530

    Create AND_tb.v

---
+++ AND_tb.v
@@ -0,0 +1,17 @@
+module AND_tb;
+
+reg a;  //1st input
+reg b;  //2nd input
+
+wire c; //output
+
+AND_dig uut(.a(a), .b(b), .c(c));
+
+initial
+begin
+    a=0;b=0;#100;
+    a=0;b=1;#100;
+    a=1;b=0;#100;
+    a=1;b=1;#100;
+end
+endmodule
