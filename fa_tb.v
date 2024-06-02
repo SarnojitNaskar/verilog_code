fd4e1d6d855424dbf693b74ea1fb619238f0d412
SarnojitNaskar <123971203+SarnojitNaskar@users.noreply.github.com>
2024-06-02 09:34:14 +0530

    Create fa_tb.v (full adder)

---
+++ fa_tb.v
@@ -0,0 +1,15 @@
+module fa_tb;
+reg [2:0]a;
+wire s, c;
+fa_dig uut (.a(a), .s(s), .c(c));
+initial begin
+    a=0;#100;
+    a=1;#100;
+    a=2;#100;
+    a=3;#100;
+    a=4;#100;
+    a=5;#100;
+    a=6;#100;
+    a=7;#100;
+end
+endmodule
